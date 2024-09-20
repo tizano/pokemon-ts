c2a1600d9a0aaf4ec0ab54fdaf772e2e585466f7
Mathieu <ma.scarlatella@gmail.com>
2024-09-20 19:10:58 -0400

    feat: add migration sql

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { env } from '@/env/server';
 import { Config, defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './src/db/schema.ts',
+  schema: './src/db/schema/schema.ts',
   dialect: 'postgresql',
   out: './src/db/migrations',
   dbCredentials: {
