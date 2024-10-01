a3c88a78ac3fb2befd5b1c3b7b6681b0509f6d5e
Mathieu <ma.scarlatella@gmail.com>
2024-10-01 00:01:54 -0400

    fix: remove unused enums

--- src/shared/enums/rarity.enum.ts
+++
@@ -1,7 +0,0 @@
-import { RarityEnum } from '@/shared/types/schema.type';
-
-export const rarityFrenchMapping: Record<string, RarityEnum> = {
-  commune: 'common',
-  'peu commune': 'uncommon',
-  rare: 'rare',
-} as const;
