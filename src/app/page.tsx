cad920e4b0c03b37f9407ba2f3d12891f1c86142
Mathieu <ma.scarlatella@gmail.com>
2024-10-02 21:45:56 -0400

    fix: add suspense boundary to fix ci build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,15 @@
 import Loader from '@/components/loader/loader';
 import { Pokemons } from '@/features/pokemons/pokemons';
 import useIntro from '@/hooks/use-intro';
+import { Suspense } from 'react';
 
 export default function Page() {
   const showIntro = useIntro();
 
   return (
-    <>
+    <Suspense>
       {showIntro && <Loader />}
       <Pokemons />
-    </>
+    </Suspense>
   );
 }
