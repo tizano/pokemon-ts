302f6f8f1d8d052748f283ce8ad0462641c9b466
Mathieu <ma.scarlatella@gmail.com>
2024-10-09 13:18:36 -0400

    style: update z-index and style animation

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -26,7 +26,7 @@ const MODAL_CONTENTS = [
   },
 ];
 
-export const Header = () => {
+export const Header = ({ className }: { className?: string }) => {
   const [isHidden, setIsHidden] = useState(false);
   const [isScrollTop, setIsScrollTop] = useState(false);
   const { scrollY } = useScroll();
@@ -52,13 +52,14 @@ export const Header = () => {
         variants={{ hidden: { y: '-100%' }, visible: { y: '0%' } }}
         transition={{ duration: 0.25 }}
         className={cn(
-          'fixed top-0 left-0 right-0 w-full z-20 backdrop-filter transition-colors',
+          'fixed top-0 left-0 right-0 w-full backdrop-filter transition-colors',
           isScrollTop ? blurClassName : '',
+          className,
         )}
       >
         <Container htmlTag="div" className="relative">
           <div className={cn('flex items-center justify-between transition-all', isScrollTop ? 'p-0' : 'py-4')}>
-            <Link href="/">
+            <Link href="/" className="motion-preset-slide-down">
               <Image src="/loader/pokemon-logo-black.svg" alt="Pokemon logo" width={150} height={55} />
             </Link>
             <div className="flex gap-4">
@@ -71,7 +72,12 @@ export const Header = () => {
                     setOpenDialogIndex(isOpen ? index : null);
                   }}
                   trigger={
-                    <Button>
+                    <Button
+                      className={cn(
+                        'motion-preset-slide-left',
+                        `${index === 0 ? 'motion-delay-150' : 'motion-delay-300'}`,
+                      )}
+                    >
                       <Plus className="mr-2" size={16} />
                       <span className="text-xs">{modal.title}</span>
                     </Button>
